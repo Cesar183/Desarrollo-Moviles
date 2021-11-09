e7427493b3da56d007ba4c4bc4e705e89e3e8716
Cesar <cesar_a_mar_@hotmail.com>
2021-11-08 20:58:30 -0500

    Creacion del boton cerrar activityBienvenida

--- app/src/main/java/com/example/proyecto/BienvenidaActivity.kt
+++ app/src/main/java/com/example/proyecto/BienvenidaActivity.kt
@@ -2,10 +2,18 @@ package com.example.proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class BienvenidaActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class BienvenidaActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
+
+        val btnCerrar = findViewById<Button>(R.id.btnClose)
+        btnCerrar.setOnClickListener {
+            finish()
+        }
     }
 }
